cbd9c6fa597ec6b8412533524f85897169094361
19wh1a0524 <harshini@beingzero.in>
2023-02-26 14:15:27 +0530

    added db connetion

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
+const dotenv = require("dotenv")
+dotenv.config();
+
+const mongoose = require("mongoose")
+const str = process.env.MONGO_CONNECTION_STRING
+
 const express = require("express");
-
 const app = express();
-
 app.use(express.static("frontend"))
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/frontend/index.html");
 })
-
-app.listen(3000, function(){
-    console.log("server running on http://localhost:3000")
+mongoose.set('strictQuery', true)
+mongoose.connect(str, function(err){
+    if (err){
+        console.error(err)
+    }
+    else{
+        console.log("DB connection successful")
+        app.listen(3000, function(){
+            console.log("server running on http://localhost:3000")
+        })
+    }
 })
+
+
